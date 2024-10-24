8637de84f2cba662e39e31876d858a8c8d28d898
Paulo Jarschel <pfjarschel@gmail.com>
2024-10-24 11:52:15 -0300

    Change UI starting script name

---
+++ start_ui.py
@@ -0,0 +1,7 @@
+import TaperPulling
+
+app, mainwindow = TaperPulling.create_ui()
+
+# Do some stuff
+
+print(app.exec())
--- test.py
+++
@@ -1,8 +0,0 @@
-import TaperPulling
-
-#print(TaperPulling.run_ui())
-
-# or
-
-app, mainwindow = TaperPulling.create_ui()
-print(app.exec())
